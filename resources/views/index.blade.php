db9a6e85e6e66c9533fdd873f33541a8102acaf2
Stefan Stefanov <ss.business.dvem@gmail.com>
2023-07-20 18:31:03 +0300

    v0.2.3

---
+++ resources/views/index.blade.php
@@ -0,0 +1,93 @@
+<footer>
+    <div>
+        <div>
+            <h3>
+                Pages
+            </h3>
+
+            <ul>
+                <li>
+                    <a href="/">
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <a href="/blog">
+                        Blogs
+                    </a>
+                </li>
+                <li>
+                    <a href="/login">
+                        Login
+                    </a>
+                </li>
+                <li>
+                    <a href="/register">
+                        Register
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h3>
+                More Information
+            </h3>
+
+            <ul>
+                <li>
+                    <a href="/">
+                        The Team
+                    </a>
+                </li>
+                <li>
+                    <a href="/">
+                        Headquarters
+                    </a>
+                </li>
+                <li>
+                    <a href="/">
+                        About us
+                    </a>
+                </li>
+                <li>
+                    <a href="/">
+                        Contact
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h3>
+                Latest blogs
+            </h3>
+
+            <ul>
+                <li>
+                    <a href="/">
+                        Not all cookies are made the same
+                    </a>
+                </li>
+                <li>
+                    <a href="/">
+                        Software Engineering is awesome
+                    </a>
+                </li>
+                <li>
+                    <a href="/">
+                        How to learn laravel in 5 steps
+                    </a>
+                </li>
+                <li>
+                    <a href="/">
+                        PHP is the best language because..
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <p>
+        Copyright 1994-2023 S.S.
+    </p>
+</footer>
