aeecca0cb9bac8c37ee84ac6d549afbe5457e7a7
Stefan Stefanov <ss.business.dvem@gmail.com>
2023-07-21 20:56:52 +0300

    v.0.4.1

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Blog Posts</title>
+    <title>{{ config('app.name') }}</title>
     <link href="/css/bootstrap.min.css" rel="stylesheet">
     <link href="/css/sticky-footer.css" rel="stylesheet">
 </head>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,31 @@
 <?php
 
-use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
-#Route::resource('/blog', PostsController::class);
+// Home Page
+Route::get('/', [\App\Http\Controllers\PagesController::class, 'index']);
 
-Route::get('/', [PagesController::class, 'index']);
-
+// Blog Page
+Route::get('/blog_main', function () {return view('welcome');});
 Route::get('/blog', [\App\Http\Controllers\PostsController::class, 'index']);
 Route::get('/blog/{blogPost}', [\App\Http\Controllers\PostsController::class, 'show']);
 
+// Create New Blogs
+Route::get('/blog/create/post', [\App\Http\Controllers\PostsController::class, 'create']);
+Route::post('/blog/create/post', [\App\Http\Controllers\PostsController::class, 'store']);
+
+// Edit Blogs
+Route::get('/blog/{blogPost}/edit', [\App\Http\Controllers\PostsController::class, 'edit']);
+Route::put('/blog/{blogPost}/edit', [\App\Http\Controllers\PostsController::class, 'update']);
+
+// Remove Blogs
+Route::delete('/blog/{blogPost}', [\App\Http\Controllers\PostsController::class, 'destroy']);
+
+// Register
 Route::get('/register', 'App\Http\Controllers\RegistrationController@create');
 Route::post('/register', 'App\Http\Controllers\RegistrationController@store');
 
+// Session
 Route::get('/login', 'App\Http\Controllers\SessionsController@create');
 Route::post('/login', 'App\Http\Controllers\SessionsController@store');
 Route::get('/logout', 'App\Http\Controllers\SessionsController@destroy');
